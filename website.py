1fc47be1a28096b694062ff2f0fdb51992196c45
chandnav <chandanavenkatesh632@gmail.com>
2024-04-29 16:39:58 +0530

    creating a new wesite.

---
+++ website.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout='wide')
+
+st.header("The Best Company")
+st.write("""A company description is an overview of the company's plan,
+            vision, and relationships. These documents typically include
+            the company's name, business structure, mission statement,
+            and an overview of the target market.""")
+
+st.subheader("Our Team")
+
+col1, col2, col3 = st.columns(3)
+
+df = pandas.read_csv("data.csv")
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
